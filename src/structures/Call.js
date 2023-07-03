dd1cb84d547c0b53b43394b9fb27dc41d419b262
caliphdev <admin@caliph.my.id>
2023-07-03 20:49:10 +0800

    Fix: Incoming Call

--- src/structures/Call.js
+++ src/structures/Call.js
@@ -18,47 +18,47 @@ class Call extends Base {
          * Call ID
          * @type {string}
          */
-        this.id = data.__x_id;
+        this.id = (data.__x_id || data.id);
         /**
          * From
          * @type {string}
          */
-        this.from = data.__x_peerJid;
+        this.from = (data.__x_peerJid?._serialized || data.peerJid
         /**
          * Unix timestamp for when the call was created
          * @type {number}
          */
-        this.timestamp = data.__x_offerTime;
+        this.timestamp = (data.__x_offerTime || data.offerTime);
         /**
          * Is video
          * @type {boolean}
          */
-        this.isVideo = data.__x_isVideo;
+        this.isVideo = data.__x_isVideo || data.isVideo;
         /**
          * Is Group
          * @type {boolean}
          */
-        this.isGroup = Boolean(typeof data.__x_isGroup == "boolean");
+        this.isGroup = Boolean(data.__x_isGroup || data.isGroup);
         /**
          * Indicates if the call was sent by the current user
          * @type {boolean}
          */
-        this.fromMe = Boolean(typeof data.__x_outgoing == "boolean");
+        this.fromMe = Boolean(data.__x_outgoing || data.outgoing);
         /**
          * Indicates if the call can be handled in waweb
          * @type {boolean}
          */
-        this.canHandleLocally = data.__x_canHandleLocally;
+        this.canHandleLocally = (data.__x_canHandleLocally || data.canHandleLocally);
         /**
          * Indicates if the call Should be handled in waweb
          * @type {boolean}
          */
-        this.webClientShouldHandle = Boolean(typeof data.__x_webClientShouldHandle == "boolean");
+        this.webClientShouldHandle = Boolean(data.webClientShouldHandle);
         /**
          * 
          * @type {string}
          */
-        this.state = data.__x__state;
+        this.state = (data.__x__state || data._state);
         /**
          * Object with participants
          * @type {object}
@@ -74,7 +74,7 @@ class Call extends Base {
     async reject() {
         return this.client.pupPage.evaluate(({ peerJid, id }) => {
             return window.WWebJS.rejectCall(peerJid, id);
-        }, { peerJid: this.from._serialized, id: this.id });
+        }, { peerJid: this.from, id: this.id });
     }
 }
 
